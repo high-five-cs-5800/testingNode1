62fc5112018b7764ed90ae343d851992fd918b1c
EC2 Default User <ec2-user@ip-172-31-37-120.ec2.internal>
2018-09-30 00:52:56 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+let methodOverride = require('method-override');
+let _ = require('lodash');
+let path = require('path');
+//create the applicaiton
+let app = express();
+
+let fs = require('fs');
+let http  = require('http');
+let https = require('https');
+
+let privateKey  = fs.readFileSync('./privatekey.pem', 'utf8');
+let certificate = fs.readFileSync('./server.crt', 'utf8');
+let credentials = {key: privateKey, cert: certificate};
+
+//Add middleware necessary for REST API's
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+
+
+
+//CORS Support
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
+app.use('/hello', function(req, res, next){
+    res.send('Hellow World');
+    next();
+});
+
+app.use('/fb', function(req, res, next){
+   res.sendFile(path.join(__dirname + '/fb.html'));
+});
+
+let  httpServer = http.createServer(app);
+let httpsServer = https.createServer(credentials, app);
+
+//connect to mogodb
+mongoose.connect('mongodb://localhost/meanapp');
+mongoose.connection.once('open', function(){
+    //load the models ...
+    app.models = require('./models/index');
+    //load the routes.
+    let routes = require('./routes');
+    _.each(routes, function(controller, route){
+        app.use(route, controller(app, route));
+    });
+   console.log('listening on port 3000...');
+   httpsServer = httpsServer.listen(3000); 
+});
