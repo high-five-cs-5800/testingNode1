9239a3405de06f797efc8507df9e83d93b383821
bonniengu <36095155+bonniengu@users.noreply.github.com>
2018-10-02 01:20:09 -0700

    testing csv

--- index.js
+++ index.js
@@ -30,6 +30,42 @@ var LogStream = fs.createWriteStream(path.join(__dirname,'morgan.log'),  {flags:
 var fs = require("fs"),
     util = require("util"),
     events = require("events");
+//testing csv to array
+function CSVToArray( strData, strDelimiter ){
+	strDelimiter = (strDelimiter || ",");
+	var objPattern = new RegExp(
+		(
+			"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+			"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+			"([^\"\\" + strDelimiter + "\\r\\n]*))"
+		),
+		"gi"
+		);
+    
+	var arrData = [[]];
+	var arrMatches = null;
+	while (arrMatches = objPattern.exec( strData )){
+		var strMatchedDelimiter = arrMatches[ 1 ];
+		if (
+			strMatchedDelimiter.length &&
+			(strMatchedDelimiter != strDelimiter)
+			){
+			arrData.push( [] );
+		}
+		if (arrMatches[ 2 ]){
+			var strMatchedValue = arrMatches[ 2 ].replace(
+				new RegExp( "\"\"", "g" ),
+				"\""
+				);
+
+		} else {
+			var strMatchedValue = arrMatches[ 3 ];
+
+		}
+		arrData[ arrData.length - 1 ].push( strMatchedValue );
+	}
+	return( arrData );
+}
 
 //CORS Support
 app.use(function(req, res, next){
